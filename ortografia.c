cf3abc07bf3eafb3ecef41786e5ade158c0fe50d
“Mateus <mateuspenaaguiar@gmail.com>
2025-03-13 15:48:19 +0000

    added multiple dictionary concatenation functionality, prob need to remove later

--- ortografia.c
+++ ortografia.c
@@ -1,9 +1,12 @@
+#define DICT_PATHS "/usr/share/dict"
+
+#include <dirent.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
 //decidimos adiantar esta parte
-char** dicionario(char * filename, int * words_len) {
+char ** dicionario(char * filename, int * words_len) {
 	FILE *file = fopen(filename, "r");
 	if (file == NULL) {
 		printf("Erro ao abrir o arquivo\n");
@@ -44,26 +47,73 @@ char** dicionario(char * filename, int * words_len) {
 	return words;
 }
 
+//esta helper function ajuda a juntar todos os dicionarios
+char ** todos_dicionarios(int * words_len) {
+	const char *dict_dir = DICT_PATHS;
+	DIR *dir = opendir(dict_dir);
+	if (dir == NULL) {
+		printf("Erro ao abrir o diretorio\n");
+		exit(1);
+	}
+
+	*words_len = 0;
+	char ** words = NULL;
+
+	// confesso que nao sabia o que era o dirent entao copiei um pouco do stack overflow
+	struct dirent *entry;
+	while ((entry = readdir(dir)) != NULL) {
+		if (strcmp(entry->d_name, ".") == 0 || strcmp(entry->d_name, "..") == 0) {
+			continue;
+		}
+
+		char filename[400];
+		snprintf(filename, sizeof(filename), "%s/%s", dict_dir, entry->d_name);
+
+		int count;
+		char ** singular = dicionario(filename, &count);
+
+		words = (char**) realloc(words, (*words_len + count) * sizeof(char*));
+		for (int i = 0; i < count; i++) {
+			words[*words_len + i] = singular[i];
+		}
+		*words_len += count;
+		free(singular);
+	}
+	closedir(dir);
+	return words;
+}
+
 void testar_linhas(char ** linhas, int nr_linhas, char ** dicionario, size_t size_dicionario, int (*compar)(const void *, const void *)) {
+	// wtf é feff
+	char * delimitadores = " \t\n/.,;:!?\"-\'()[]{}﻿";
 	int primeira = 1;
 	for (int i = 0; i < nr_linhas; i++) {
 		char * linha = strdup(linhas[i]);
 		// para nao estragar a linha original tenho de duplicar a linha
-		char * pch = strtok(linha, " \t\n/.,;:!?\"-");
+		char * pch = strtok(linha, delimitadores);
 		// o strtok divide a linha pelos separadores que definimos dentro das aspas
 		while (pch != NULL) {
-			// o bsearch procura a palavra no dicionario
-			void * res = bsearch(&pch, dicionario, size_dicionario, sizeof(char*), compar);
+			//tive de adicionar logica para lidar com numeros, caso o atoi seja 0. Ainda há o caso de haver uma palavra com multiplos 000000 mas vou ignorar isso
+			if (*pch == '0') {
+				pch = strtok(NULL, delimitadores);
+				continue;
+			} else if (atoi(pch) != 0) {
+				pch = strtok(NULL, delimitadores);
+				continue;
+			} else {
+				// o bsearch procura a palavra no dicionario
+				void * res = bsearch(&pch, dicionario, size_dicionario, sizeof(char*), compar);
 
-			if (res == NULL) {
-				if (primeira == 1) {
-					printf("%d: %s\n", i+1, linhas[i]);
-					primeira = 0;
+				if (res == NULL) {
+					if (primeira == 1) {
+						printf("%d: %s\n", i+1, linhas[i]);
+						primeira = 0;
+					}
+					printf("Erro na palavra \"%s\"\n", pch);
 				}
-				printf("Erro na palavra \"%s\"\n", pch);
+				
+				pch = strtok(NULL, " \t\n/.,;:!?\"-\'()[]{}");
 			}
-			
-			pch = strtok(NULL, " \t\n/.,;:!?\"-");
 		}
 		free(linha);
 		primeira = 1;
@@ -78,11 +128,9 @@ int compare_strings(const void *a, const void *b) {
     return strcasecmp(str1, str2);
 }
 
-int main(int argc, char **argv) {
-	char* filename = "/usr/share/dict/words";
-
+int main(int argc, char ** argv) {
 	int words_len; 
-	char** words = dicionario(filename, &words_len);
+	char ** words = todos_dicionarios(&words_len);
 
 	qsort(words, words_len, sizeof(char*), compare_strings);	
 
