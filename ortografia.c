d26ecfe3b3b6b0b6f7b828f64571a1112f29dfd4
“Mateus <mateuspenaaguiar@gmail.com>
2025-03-13 00:37:43 +0000

    handling input

--- ortografia.c
+++ ortografia.c
@@ -3,7 +3,7 @@
 #include <string.h>
 
 //decidimos adiantar esta parte
-char** dicionario(char* filename) {
+char** dicionario(char * filename, int * words_len) {
 	FILE *file = fopen(filename, "r");
 	if (file == NULL) {
 		printf("Erro ao abrir o arquivo\n");
@@ -22,7 +22,7 @@ char** dicionario(char* filename) {
 
 	rewind(file);
 
-	char **words = (char**) malloc(count * sizeof(char*));
+	char ** words = (char**) malloc(count * sizeof(char*));
 
 	int i = 0;
 	while (fgets(buffer, sizeof(buffer), file)) {
@@ -34,21 +34,81 @@ char** dicionario(char* filename) {
 
 		// aloca a palavra e copia o buffer para a palavra
 		// desta forma cada palavra tem o espaco certo
-		words[i] = (char*) malloc(len);
+		words[i] = (char*) malloc(len + 1);
 		strcpy(words[i], buffer);
 		i++;
 	}
 	fclose(file);
+
+	*words_len = count;
 	return words;
+}
+
+//resolver o problema da funcao, vi no stack overflow
+int compare_strings(const void *a, const void *b) {
+    const char *str1 = *(const char **)a;
+    const char *str2 = *(const char **)b;
+    
+    return strcasecmp(str1, str2);
 }
 
 int main(int argc, char **argv) {
 	char* filename = "/usr/share/dict/words";
 
-	char** words = dicionario(filename);
-	
-	// imprime as palavras
-	for (int i = 0; i < 10; i++) {
-		printf("%s\n", words[i]);
+	int words_len; 
+	char** words = dicionario(filename, &words_len);
+
+	qsort(words, words_len, sizeof(char*), compare_strings);	
+
+/* PARTE DO INPUT */
+	char ** lines = NULL;
+	char buffer[1000];
+
+	int linhasalocadas = 0;
+	int count = 0;
+
+	while (fgets(buffer, sizeof(buffer), stdin) != NULL) {
+		size_t linha_len = strlen(buffer);	
+		if (linha_len > 0 && buffer[linha_len - 1] == '\n') {
+			buffer[linha_len - 1] = '\0';
+			linha_len--;
+		}
+
+		if (count >= linhasalocadas) {
+			linhasalocadas += 10;
+			lines = (char**) realloc(lines, linhasalocadas * sizeof(char*));
+		}
+
+		lines[count] = (char*) malloc(linha_len + 1);
+		if (lines[count] == NULL) {
+			printf("Erro ao alocar memoria\n");
+			free(lines);
+			for (int j = 0; j < count; j++) {
+				free(lines[j]);
+			}
+			exit(1);
+		}
+		strcpy(lines[count], buffer);
+		count++;
 	}
+
+	for (int i = 0; i < count; i++) {
+		printf("%s\n", lines[i]);
+	}
+
+  /* FREES */
+	for (int j = 0; j < words_len; j++) {
+		free(words[j]);
+	}
+
+	free(words);
+  
+	for (int j = 0; j < count; j++) {
+		free(lines[j]);
+	}
+
+	free(lines);
+
+	return 0;
+
 }
